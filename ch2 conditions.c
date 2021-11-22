2e46399c735bf4ee35c6203a0a1324eab87221b8
hatim <rouchdi.hatim@gmail.com>
2021-11-22 14:36:51 +0100

    challenge-conditions

---
+++ ch2 conditions.c
@@ -0,0 +1,9 @@
+void echange(int a , int b){
+
+     printf(" a = %d, b = %d\n" , a, b);
+     int
+     z=a;
+     a=b;
+     b=z;
+     printf("a=%d , b=%\n" ,a,b);
+     }
---
+++ ch3 conditions.c
@@ -0,0 +1,34 @@
+#define true 1 
+#define false 2
+
+typedef int bool
+
+ bool isPremier(int nombbre){
+ 	
+ 	int yes = nombre;
+ 	int no = 0;
+ 	
+ while (nombre > 0){
+ 	if( nombre % yes == 0)
+ 	 no++;
+ 	 nombre++;
+ 	
+ }
+  if (no != 2)
+    return false;
+    else
+    return true;
+    
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ }
